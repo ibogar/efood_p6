e91f97e19405346de7cfdbb81dcda1c131c58b89
Iuri Guilherme Bogar Portilho <iuribogar@hotmail.com>
2025-10-14 18:43:05 -0300

    reboot setup with vite

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react"
+
+const App = () => (
+    <>
+      <div>
+        <p>
+          Click on the Vite and React logos to learn more
+        </p>
+      </div>
+
+
+    </>
+  )
+
+export default App
--- src/App.tsx
+++
@@ -1,9 +0,0 @@
-function App() {
-  return (
-    <>
-      <div>ola</div>
-    </>
-  )
-}
-
-export default App
